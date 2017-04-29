6d4adb44ea6b86ed59a8e6ea64a98843e8a10087
agar88 <agrawind@yandex.ru>
2017-04-29 11:03:13 +0500

    fixes

--- src/games/even.js
+++ src/games/even.js
@@ -5,8 +5,9 @@ const rules = 'Answer "yes" if number even otherwise answer "no".';
 
 const game = () => {
   const number = makeRandomNum(1, 100);
+  const problem = String(number);
   const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-  return [number, correctAnswer];
+  return [problem, correctAnswer];
 };
 
 export default () => gameFlow(rules, game);
