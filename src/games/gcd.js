8cc320bf4abd00442d0954b61164f03be61ace13
agar88 <agrawind@yandex.ru>
2017-05-04 20:32:06 +0500

    small refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,20 +3,19 @@ import makeRandomNum from '../utils';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const getGcd = (num1, num2) => {
+  if (num1 === num2) {
+    return num1;
+  }
+  const difference = Math.abs(num1 - num2);
+  return num1 > num2 ? getGcd(difference, num2) : getGcd(num1, difference);
+};
+
 const game = () => {
   const number1 = makeRandomNum(1, 100);
   const number2 = makeRandomNum(1, 100);
-
-  const iter = (num1, num2) => {
-    if (num1 === num2) {
-      return num1;
-    }
-    const difference = Math.abs(num1 - num2);
-    return num1 > num2 ? iter(difference, num2) : iter(num1, difference);
-  };
-
   const question = `${number1} ${number2}`;
-  const correctAnswer = iter(number1, number2);
+  const correctAnswer = getGcd(number1, number2);
   return [question, String(correctAnswer)];
 };
 
