e27d407619ec0564e8d7416a29a320630da87dd6
agar88 <agrawind@yandex.ru>
2017-05-04 20:33:08 +0500

    small refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,28 +3,26 @@ import makeRandomNum from '../utils';
 
 const description = 'What number is missing in this progression?';
 
-const game = () => {
+const makeProgression = () => {
   const firstNumber = makeRandomNum(1, 10);
   const stepWidth = makeRandomNum(2, 10);
-  const indexOfHidden = makeRandomNum(2, 9);
 
-  const makeProgression = (i, acc) => {
+  const iter = (i, acc) => {
     if (i === 9) {
       return acc;
     }
     const nextNumber = acc[i] + stepWidth;
-    return makeProgression(i + 1, acc.concat(nextNumber));
+    return iter(i + 1, acc.concat(nextNumber));
   };
 
-  const progression = makeProgression(0, [firstNumber]);
+  return iter(0, [firstNumber]);
+};
+
+const game = () => {
+  const progression = makeProgression();
+  const indexOfHidden = makeRandomNum(2, 9);
   const correctAnswer = progression[indexOfHidden];
-
-  const toHidden = (num) => {
-    const newEl = num !== correctAnswer ? num : '..';
-    return newEl;
-  };
-
-  const progressionWithHidden = progression.map(toHidden);
+  const progressionWithHidden = progression.map(num => (num !== correctAnswer ? num : '..'));
   const question = progressionWithHidden.join(' ');
 
   return [question, String(correctAnswer)];
